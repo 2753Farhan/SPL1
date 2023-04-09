cb4780d22239585a75651f5e548f0a55d4c55a32
Md Farhan Islam Shuvro <bsse1303@iit.du.ac.bd>
2023-04-09 18:17:37 +0600

    modified

--- errorbasedsqlinjection.cpp
+++ errorbasedsqlinjection.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 #include <curl/curl.h>
 
 // Callback function to store response data
@@ -24,54 +26,66 @@ int main() {
         return 1;
     }
 
-    std::string url = "http://localhost/test/login.php";
-    std::string username;
-    std::string password = "123456";
-    std::string response;
+    // URL to send requests to
+    std::string url = "http://localhost/tigernetBD-master/tgnet/login.php";
 
-    std::cout << "Enter username: ";
-    std::cin >> username;
-
-    std::string payload = "username=" + username + "&password=" + password;
-
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, payload.length());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, HeaderCallback);
-    curl_easy_setopt(curl, CURLOPT_HEADERDATA, NULL);
-    curl_easy_setopt(curl, CURLOPT_HEADER, 1);
-
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-        std::cerr << "Failed to perform curl request: " << curl_easy_strerror(res) << "\n";
+    // Open the file containing payloads
+    std::ifstream infile("payloads.txt");
+    if (!infile.is_open()) {
+        std::cerr << "Failed to open payloads file\n";
         curl_easy_cleanup(curl);
         return 1;
     }
 
+    // Store the payloads in a vector
+    std::vector<std::string> payloads;
+    std::string payload;
+    while (std::getline(infile, payload)) {
+        payloads.push_back(payload);
+    }
+
+    // Close the file
+    infile.close();
+
+    std::string errrormessage="You have an error in your SQL syntax; check the manual that corresponds to your";
+    bool vulnerable=false;
+
+    // Send requests for each payload in the vector
+    for (const auto& payload : payloads) {
+        std::string response;
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, payload.length());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, HeaderCallback);
+        curl_easy_setopt(curl, CURLOPT_HEADERDATA, NULL);
+        curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            std::cerr << "Failed to perform curl request: " << curl_easy_strerror(res) << "\n";
+            curl_easy_cleanup(curl);
+            return 1;
+        }
+
+        if (response.empty()) {
+            std::cerr << "Empty response data\n";
+            continue;
+        }
+
+        std::cout << "Response: " << response << "\n";
+        if(response.find(errrormessage)!=std::string::npos){
+            vulnerable=true;
+        }
+
+    }
+
     curl_easy_cleanup(curl);
-
-    if (response.empty()) {
-        std::cerr << "Empty response data\n";
-        return 1;
+    if(vulnerable){
+        std::cout << "The website is vulnerable\n";
     }
-
-    std::cout << "Response: " << response << "\n";
-
-    const std::string success_message = "login successful";
-    const std::string failure_message = "login failed";
-    const std::string error_message = "SQL error";
-
-    if (response.find(success_message) != std::string::npos) {
-        std::cout << "Login successful\n";
-    } else if (response.find(failure_message) != std::string::npos) {
-        std::cout << "Login failed\n";
-    } else if (response.find(error_message) != std::string::npos) {
-        std::cout << "Website is vulnerable to SQL injection\n";
-    } else {
-        std::cout << "Unknown response\n";
-    }
-
+    else std::cout << "The website is not vulnerable\n";
     return 0;
 }
