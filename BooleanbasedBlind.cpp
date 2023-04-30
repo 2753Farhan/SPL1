b6593765f34b0899053a5cfbe12a2abb681ba459
Md Farhan Islam Shuvro <bsse1303@iit.du.ac.bd>
2023-04-30 08:21:14 +0600

    modified

--- BooleanbasedBlind.cpp
+++ BooleanbasedBlind.cpp
@@ -12,17 +12,21 @@ size_t WriteCallback(char *ptr, size_t size, size_t nmemb, std::string *userdata
     return len;
 }
 
-std::vector<int> compute_prefix(const std::string& pattern) {
+std::vector<int> compute_prefix(const std::string& pattern)
+{
     int n = pattern.size();
     std::vector<int> prefix(n);
     prefix[0] = 0;
 
     int j = 0;
-    for (int i = 1; i < n; i++) {
-        while (j > 0 && pattern[j] != pattern[i]) {
+    for (int i = 1; i < n; i++)
+    {
+        while (j > 0 && pattern[j] != pattern[i])
+        {
             j = prefix[j-1];
         }
-        if (pattern[j] == pattern[i]) {
+        if (pattern[j] == pattern[i])
+        {
             j++;
         }
         prefix[i] = j;
@@ -32,7 +36,8 @@ std::vector<int> compute_prefix(const std::string& pattern) {
 }
 
 // Search for the pattern within the text using the KMP algorithm
-size_t Find(const std::string& text, const std::string& pattern) {
+size_t Find(const std::string& text, const std::string& pattern)
+{
     int n = text.size();
     int m = pattern.size();
 
@@ -40,14 +45,18 @@ size_t Find(const std::string& text, const std::string& pattern) {
     std::vector<int> prefix = compute_prefix(pattern);
 
     int j = 0;
-    for (int i = 0; i < n; i++) {
-        while (j > 0 && pattern[j] != text[i]) {
+    for (int i = 0; i < n; i++)
+    {
+        while (j > 0 && pattern[j] != text[i])
+        {
             j = prefix[j-1];
         }
-        if (pattern[j] == text[i]) {
+        if (pattern[j] == text[i])
+        {
             j++;
         }
-        if (j == m) {
+        if (j == m)
+        {
             // Found a match at position i-m+1
             return i-m+1;
         }
